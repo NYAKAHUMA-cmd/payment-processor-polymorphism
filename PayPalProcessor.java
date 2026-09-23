f20bbd9261129ac095f04525795ba47e91813edd
Hellen684 <230101045+Hellen684@users.noreply.github.com>
2026-09-23 16:15:57 +0300

    PayPalProcessor added

---
+++ PayPalProcessor.java
@@ -0,0 +1,16 @@
+// Subclass - overrides processPayment
+public class PayPalProcessor extends PaymentProcessor {
+    
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PayPal: Sending $" + amount + " via PayPal account.");
+    }
+    
+    // Overloaded method (compile-time polymorphism)
+    public void processPayment(double amount, String couponCode) {
+        double discount = amount * 0.15; // 15% off
+        double finalAmount = amount - discount;
+        System.out.println("PayPal: Sending $" + finalAmount 
+            + " via PayPal (Coupon '" + couponCode + "' applied, saved $" + discount + ").");
+    }
+}
